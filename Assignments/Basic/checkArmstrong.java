fdc2bbd6da95f7236157de0d2acab6c3798409f8
Amandeep <amandeepakuramuadk@gmail.com>
2024-11-17 19:44:41 +0530

    check for armstrong number file added

--- Assignments/Basic/checkArmstrong.java
+++ Assignments/Basic/checkArmstrong.java
@@ -2,6 +2,30 @@
 public class checkArmstrong {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
         int n = in.nextInt();
+        int original = n;
+        // let say this is only for 3 digits
+        double rem, sum = 0;
+        // now I have to calculate the power which is the no of digits in the number
+        // int power = 0;
+        int temp = n;
+        int count = 0;
+        while(temp !=0){
+            rem = n%10;
+            count++;
+            temp/=10;
+        }
+        
+        while(n!=0){
+            rem = n%10;
+            sum += Math.pow(rem, count);
+            n /= 10;
+        } 
+        if(sum == original){
+            System.out.println(original+ " is a armstrong number");
+        }else{
+            System.out.println(original+" is not a armstrong number");
+        }
     }
 }
